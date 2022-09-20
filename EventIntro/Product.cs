018df22e5df71523f0765e6a4e3e0f73b3340dbe
eda.bezek <eda_bezek@hotmail.com>
2022-09-20 16:24:02 +0300

    Events added

---
+++ EventIntro/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventIntro
+{
+    public delegate void StockControl();
+    public class Product
+    {
+        int _stock;
+        public Product(int stock)
+        {
+            _stock = stock; 
+        }
+        public event StockControl StockControlEvent;
+        public string ProductName { get; set; }
+        public int Stock
+        { 
+            //stogu okuyoruz
+            get { 
+                return _stock; 
+            } 
+            //stogu set edebiliyoruz ve koşullar varsa event çalışıyor
+            set { 
+                _stock = value;
+                if (value<=15 && StockControlEvent != null)//stok 15den az kaldığında bizi uyarsın ve stok kontolde abone mi kontol ediyoruz.--evente abone olmuş mu
+                {
+                    StockControlEvent();// eventı tetikliyoruz
+                }
+            }
+        }  
+        public void Sell(int amount)
+        {
+            Stock -= amount;
+            Console.WriteLine("{1} Stock amount : {0}", Stock,ProductName); 
+        }
+    }
+}
