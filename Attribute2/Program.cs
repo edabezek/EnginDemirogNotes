e376fc03d1aa9ae490d6a01cb2e682d0cce38aa8
eda.bezek <eda_bezek@hotmail.com>
2022-09-20 11:07:25 +0300

    Reflections added

---
+++ Attribute2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25._2_Attribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer { Id = 1, LastName = "Demiroğ", Age = 32 };
+
+            CustomerDal customerDal = new CustomerDal();
+            customerDal.Add(customer);
+
+            Console.ReadLine();
+        }
+
+    }
+
+    [ToTable("Customers")]
+    //[ToTable("Products")]
+
+    class Customer
+    {
+        public int Id { get; set; }
+        [RequiredProperty]
+        [RequiredProperty]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+    }
+    class CustomerDal
+    {
+        [Obsolete("Don't use Add,instead use AddNew Method")]
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("{0},{1},{2},{3} added!", customer.Id, customer.FirstName, customer.LastName, customer.Age);
+        }
+        public void AddNew(Customer customer)
+        {
+            Console.WriteLine("{0},{1},{2},{3} added!", customer.Id, customer.FirstName, customer.LastName, customer.Age);
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]//all hepsinde kullanılabilir. Class sadece clas üstü kullanılabilir.Property dersek sadece property için kullanılır.
+    // [AttributeUsage(AttributeTargets.All | AttributeTargets.Field)] gibi de yazılabilir.
+    //AllowMultiple birden fazla yerde  kullanım için,mesela last name e iki defa uygulanabilir.yada to table üstüne yazılan gibi çoğaltmak için [ToTable("Products")] böylece iki tablo için geçerli olur.
+    class RequiredPropertyAttribute : Attribute
+    {
+
+    }
+    class ToTable : Attribute
+    {
+        private string v;
+
+        public ToTable(string v)
+        {
+            this.v = v;
+        }
+    }
+
+
+
+
+}
---
+++ Giriş/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Giriş
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //DortIslem dortIslem = new DortIslem(2, 3);
+            //dortIslem.Topla(2, 2);//kendi verdiğimiz değer ile çalışacak
+            //dortIslem.Topla2();//değer yazmayız constructordaki değerleden çalışır.(2,3) ile
+
+            //bunu runtime ile yapmak istersek -reflection kullanmak 
+            var type= typeof(DortIslem);//burada bizim çalışacağımız bir tip var o da 4 işlem -bu instance alma değil tipini alıyoruz- 
+
+            DortIslem dortIslem =(DortIslem)Activator.CreateInstance(type);//bununla instance almak aynı şey-burada gelen tipe göre almış oluyoruz. (Activator obje döner-bu yüzden cast yaptık.)
+            dortIslem.Topla(2, 2);
+
+            DortIslem dortIslem2 = (DortIslem)Activator.CreateInstance(type,6,7); 
+            dortIslem2.Topla2(); 
+        }
+    }
+    public class DortIslem
+    {
+        int _sayi1;
+        int _sayi2;
+        public DortIslem(int sayi1,int sayi2)//kullanıcıdan aldığımız yer burası
+        {
+            _sayi1=sayi1;   
+            _sayi2=sayi2;   
+        }
+        public DortIslem()
+        {
+                
+        }
+        public int Topla(int sayi1, int sayi2) 
+        {
+            return sayi1 + sayi2;//kullanıcıdan girilmesini ister.
+        }
+        public int Carp(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+        public int Topla2()
+        {
+            return _sayi1 + _sayi2;//constructordan gelen değişkenleri set eder
+        }
+        public int Carp2()
+        {
+            return _sayi1 * _sayi2;
+        }
+
+    }
+}
+//reflectionda çalışma anında-runtime bilgi toplanabilir
+//runtime-çalışma anında nesnelerimiz hakkında bilgi almak ve bu nesneleri çalıştırabilmek için kullanılır.
